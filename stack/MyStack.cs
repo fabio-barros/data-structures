ac727b5472a7bb465edd15e9a5f29ea7a09bfe90
Fábio Barros Ribeiro <whatupfabio@gmail.com>
2021-10-01 07:17:21 -0300

    stack

---
+++ stack/MyStack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace stack
+{
+    public class MyStack
+    {
+        static readonly int MAX_SIZE = 100;
+        int top;
+        int[] stack = new int[MAX_SIZE];
+
+
+        public MyStack()
+        {
+            top = -1;
+        }
+
+        public bool Push(int value)
+        {
+            if (top >= MAX_SIZE)
+            {
+                Console.WriteLine("Stack Overflow");
+                return false;
+            }
+            else
+            {
+                stack[++top] = value;
+                return true;
+            }
+        }
+
+        public int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return 0;
+            }
+            else
+            {
+                int value = stack[top--];
+                return value;
+            }
+        }
+
+        public void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+                Console.WriteLine("Elemento no topo : {0}", stack[top]);
+        }
+
+        bool IsEmpty()
+        {
+            return (top < 0);
+        }
+
+        public void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Elementos no stacj :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+
+}
---
+++ stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+
+            myStack.Push(5);
+            myStack.Push(10);
+            myStack.Push(15);
+            myStack.Push(20);
+
+            myStack.PrintStack();
+
+            myStack.Peek();
+
+            Console.WriteLine("Item removido : {0}", myStack.Pop());
+
+            myStack.PrintStack();
+
+            Stack Stack = new Stack();
+            Stack.Push("g");
+            Stack.Push("f");
+            Stack.Push("t");
+
+            PrintValues(Stack);
+
+
+        }
+
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
